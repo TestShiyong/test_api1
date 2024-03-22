d36198c1d57c7e0d2ff21ea0be388494a52dff59
shiyong <shiyong@gaoyaya.com>
2024-03-22 10:55:01 +0800

    Changes

--- test_job/az_test_job.py
+++ test_job/az_test_job.py
@@ -3,7 +3,19 @@ from common.handle_database import Database
 import main
 
 
-def group_goods(url):
+
+def group_list(l1):
+    l2 = []
+    for goods in l1:
+        if goods not in l2:
+            l2.append(goods)
+    return l2
+
+
+import requests
+
+
+def group_goods(url, page_numbers):
     """
     列表页接口返回数据 判断列表goods是否有重复
     :return:
@@ -16,20 +28,22 @@ def group_goods(url):
               "x-countryCode": "US",
               "authorization": "Basic bGViYmF5OnBhc3N3MHJk"
               }
-    data = {"filters": {}, "view_mode": ["petite"], "originUrl": "/all/flower-girl-dresses?sort_by=popularity&page=1"}
+    data = {"filters": {}, "view_mode": ["petite"], "originUrl": url}
 
     goods_list = []
-    for i in range(1, 7):
-        url = url
-        res = requests.post(url, json=data, headers=header)
+    for number in range(*page_numbers):
+        url_with_page = url.replace("page=1", f"page={number}")
+        res = requests.post(url_with_page, json=data, headers=header)
         dict1 = res.json()['data']['prodList']
-        for i in dict1:
-            goods_list.append(i['goodsId'])
-    no_goods = set(goods_list)
-    print(len(goods_list), print(goods_list))
-    for i in no_goods:
-        print(i, goods_list.count(i))
+        for item in dict1:
+            goods_list.append(item['goodsId'])
+    no_duplicates = group_list(goods_list)
+    print(f"Total number of goods: {len(goods_list)}")
+    print(goods_list)
+    for item_id in no_duplicates:
+        print(f"Item ID: {item_id}, Count: {goods_list.count(item_id)}")
     return goods_list
+
 
 
 def az_database():
@@ -96,12 +110,9 @@ def update_order_info():
                 continue
 
 
-pre_url = 'https://p6.azazie.com/pre/1.0/list/content?format=list&cat_name=flower-girl-dresses&dress_type=dress&page=1&limit=60&in_stock=&sort_by=popularity&is_outlet=0&version=b&activityVerison=a&galleryVersion=B&sodGalleryVersion=B&topic=azazie&listColorVersion=A'
-group_goods((1, 7), pro_url)
-print(
-    '-----------------------------------------------------------------------------------------------------------------------------------------------------------------')
 
 pro_url = 'https://www.azazie.com/prod/1.0/list/content?format=list&cat_name=flower-girl-dresses&dress_type=dress&page=1&limit=60&in_stock=&sort_by=popularity&is_outlet=0&version=b&activityVerison=a&galleryVersion=B&sodGalleryVersion=B&topic=azazie&listColorVersion=A'
+pre_url = 'https://p6.azazie.com/pre/1.0/list/content?format=list&cat_name=flower-girl-dresses&dress_type=dress&page=1&limit=60&in_stock=&sort_by=popularity&is_outlet=0&version=b&activityVerison=a&galleryVersion=B&sodGalleryVersion=B&topic=azazie&listColorVersion=A'
+group_goods(pro_url, (1, 7))
+group_goods(pre_url, (1, 5))
 
-group_goods(pre_url)
-# update_order_info()
