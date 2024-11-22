28e35d08df9509ed2b248ee4cee3a68b1dd60799
shiyong <shiyong@gaoyaya.com>
2024-11-22 14:49:28 +0800

    test

--- common/handle_prime_data.py
+++ common/handle_prime_data.py
@@ -1,29 +1,29 @@
-parameters.pyfrom common.dress_type import all_dress_type
-from common.handleRequest import send_request
-from common.handle_conf_file import cf
-from common.handleCsv import waite_data_cav
-import mypath
+from common.dressTypeData import all_dress_type
+from common.handleRequest import sendRequest
+from common.handleConfig import ConfigReader
+from common.handleCsv import waiteDataCav
+import myPath
 import os
 import queue
 from threading import Thread
 
 
 # def preset_data():
-#     base_url = cf.get_str('Url', 'TEST_URL')
+#     base_url = ConfigReader.get_str('Url', 'TEST_URL')
 #     # test
-#     base_url = cf.get_str('Url', 'PROD_URL')
+#     base_url = ConfigReader.get_str('Url', 'PROD_URL')
 #     # 遍历goods type
 #     for cat_name in all_dress_type:
 #         if 'sample' in cat_name[0]:
-#             url = f'/list/content?cat_name={cat_name[0]}&dress_type=sample&page=1&limit=30&page=1'
+#             url = f'/list/content?cat_name={cat_name[0]}&dressTypeData=sample&page=1&limit=30&page=1'
 #         elif 'ready-to-ship' in cat_name[0]:
-#             url = f'/list/content?cat_name={cat_name[0]}&dress_type=clearance&page=1&limit=30&page=1'
+#             url = f'/list/content?cat_name={cat_name[0]}&dressTypeData=clearance&page=1&limit=30&page=1'
 #         elif 'outlet' in cat_name[0]:
-#             url = f'/list/content?cat_name={cat_name[0]}&dress_type=outlet&page=1&limit=30&page=1'
+#             url = f'/list/content?cat_name={cat_name[0]}&dressTypeData=outlet&page=1&limit=30&page=1'
 #         else:
-#             url = f'/list/content?cat_name={cat_name[0]}&dress_type=dress&page=1&limit=30&page=1'
-#         res = send_request('post', url=base_url + url,out_put=False)
-#         path = os.path.join(mypath.prime_data_dir, cat_name[1])
+#             url = f'/list/content?cat_name={cat_name[0]}&dressTypeData=dress&page=1&limit=30&page=1'
+#         res = sendRequest('post', url=base_url + url,out_put=False)
+#         path = os.path.join(myPath.prime_data_dir, cat_name[1])
 #         product_info_list = []
 #         for i in res.json()["data"]["prodList"]:
 #             goods_id = str(i["goodsId"])
@@ -31,37 +31,37 @@ from threading import Thread
 #             cat_id = str(i["catId"])
 #             shop_price = str(i["shopPrice"])
 #             no_deal_price = str(i['noDealPrice'])
-#             dress_type = i["dressType"]
+#             dressTypeData = i["dressType"]
 #             price_symbol = i['priceSymbol']
 #             goods_sn = i['goodsSn']
 #             market_price = i['marketPrice']
 #
 #             product_info_list.append(
 #                 {"goods_id": goods_id, "goods_name": goods_name, "cat_id": cat_id, "shop_price": shop_price,
-#                  "no_deal_price": no_deal_price, "dress_type": dress_type, "price_symbol": price_symbol,
+#                  "no_deal_price": no_deal_price, "dressTypeData": dressTypeData, "price_symbol": price_symbol,
 #                  "goods_sn": goods_sn, "market_price": market_price})
-#         waite_data_cav(path,product_info_list)
+#         waiteDataCav(path,product_info_list)
 #
 #
 
 
 def preset_data_work(que):
-    base_url = cf.get_str('Url', 'TEST_URL')
+    base_url = ConfigReader.get_str('Url', 'TEST_URL')
     # test
-    base_url = cf.get_str('Url', 'PROD_URL')
+    base_url = ConfigReader.get_str('Url', 'PROD_URL')
     while que.qsize() is not None:
         cat_name = que.get(timeout=0.1)
 
         if 'sample' in cat_name[0]:
-            url = f'/list/content?cat_name={cat_name[0]}&dress_type=sample&page=1&limit=30&page=1'
+            url = f'/list/content?cat_name={cat_name[0]}&dressTypeData=sample&page=1&limit=30&page=1'
         elif 'ready-to-ship' in cat_name[0]:
-            url = f'/list/content?cat_name={cat_name[0]}&dress_type=clearance&page=1&limit=30&page=1'
+            url = f'/list/content?cat_name={cat_name[0]}&dressTypeData=clearance&page=1&limit=30&page=1'
         elif 'outlet' in cat_name[0]:
-            url = f'/list/content?cat_name={cat_name[0]}&dress_type=outlet&page=1&limit=30&page=1'
+            url = f'/list/content?cat_name={cat_name[0]}&dressTypeData=outlet&page=1&limit=30&page=1'
         else:
-            url = f'/list/content?cat_name={cat_name[0]}&dress_type=dress&page=1&limit=30&page=1'
-        res = send_request('post', url=base_url + url, out_put=False)
-        path = os.path.join(mypath.prime_data_dir, cat_name[1])
+            url = f'/list/content?cat_name={cat_name[0]}&dressTypeData=dress&page=1&limit=30&page=1'
+        res = sendRequest('post', url=base_url + url, out_put=False)
+        path = os.path.join(myPath.prime_data_dir, cat_name[1])
         product_info_list = []
         for i in res.json()["data"]["prodList"]:
             goods_id = str(i["goodsId"])
@@ -69,16 +69,16 @@ def preset_data_work(que):
             cat_id = str(i["catId"])
             shop_price = str(i["shopPrice"])
             no_deal_price = str(i['noDealPrice'])
-            dress_type = i["dressType"]
+            dressTypeData = i["dressType"]
             price_symbol = i['priceSymbol']
             goods_sn = i['goodsSn']
             market_price = i['marketPrice']
 
             product_info_list.append(
                 {"goods_id": goods_id, "goods_name": goods_name, "cat_id": cat_id, "shop_price": shop_price,
-                 "no_deal_price": no_deal_price, "dress_type": dress_type, "price_symbol": price_symbol,
+                 "no_deal_price": no_deal_price, "dressTypeData": dressTypeData, "price_symbol": price_symbol,
                  "goods_sn": goods_sn, "market_price": market_price})
-        waite_data_cav(path, product_info_list)
+        waiteDataCav(path, product_info_list)
 
 
 if __name__ == '__main__':
